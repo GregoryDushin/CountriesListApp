9f8300a4d6f8c5cc5997533b482274ef36309486
GrigoryDushin <g.s.dushin@gmail.com>
2023-12-22 20:19:22 +0100

    Presenter troubles

--- CountriesListApp/CountriesList/Networking/CountriesLoader.swift
+++ CountriesListApp/CountriesList/Networking/CountriesLoader.swift
@@ -7,30 +7,68 @@
 
 import Foundation
 
-final class CountryLoader: CountryLoaderProtocol {
+//final class CountryLoader: CountryLoaderProtocol {
+//
+//    private let decoder = JSONDecoder()
+//    private let session = URLSession.shared
+//    
+//    init() {
+//        decoder.keyDecodingStrategy = .convertFromSnakeCase
+//    }
+//    
+//    func countryDataLoad(completion: @escaping (Result<[Country], Error>) -> Void) {
+//        loadCountryData(url: URL(string: Url.countriesUrl), completion: completion)
+//    }
+//    
+//    private func loadCountryData(url: URL?, completion: @escaping (Result<[Country], Error>) -> Void) {
+//        guard let url else { return }
+//
+//        let task = session.dataTask(with: url) { data, _, error in
+//            guard let data else { return }
+//            
+//            do {
+//                let json = try self.decoder.decode(CountryResponse.self, from: data)
+//                let countries = json.countries
+//                completion(.success(countries))
+//
+//            } catch {
+//                completion(.failure(error))
+//            }
+//        }
+//        task.resume()
+//    }
+//}
 
+// MARK: Trying to make universal Loader
+
+protocol DataLoadable {
+    associatedtype DataType: Decodable
+    func loadData(completion: @escaping (Result<[DataType], Error>) -> Void)
+}
+
+final class DataLoader<T: Decodable>: DataLoadable {
     private let decoder = JSONDecoder()
     private let session = URLSession.shared
-    
-    init() {
+    private let apiUrl: String
+
+    init(apiUrl: String) {
+        self.apiUrl = apiUrl
         decoder.keyDecodingStrategy = .convertFromSnakeCase
     }
-    
-    func countryDataLoad(completion: @escaping (Result<[Country], Error>) -> Void) {
-        loadCountryData(url: URL(string: Url.countriesUrl), completion: completion)
-    }
-    
-    private func loadCountryData(url: URL?, completion: @escaping (Result<[Country], Error>) -> Void) {
-        guard let url else { return }
+
+    func loadData(completion: @escaping (Result<[T], Error>) -> Void) {
+        guard let url = URL(string: apiUrl) else {
+            return
+        }
 
         let task = session.dataTask(with: url) { data, _, error in
-            guard let data else { return }
-            
-            do {
-                let json = try self.decoder.decode(CountryResponse.self, from: data)
-                let countries = json.countries
-                completion(.success(countries))
+            guard let data else {
+                return
+            }
 
+            do {
+                let items = try self.decoder.decode([T].self, from: data)
+                completion(.success(items))
             } catch {
                 completion(.failure(error))
             }
--- CountriesListApp/CountriesList/Presenter/CountriesListPresenter.swift
+++ CountriesListApp/CountriesList/Presenter/CountriesListPresenter.swift
@@ -7,27 +7,66 @@
 
 import Foundation
 
-final class CountriesListPresenter: CountriesListPresenterProtocol {
+//final class CountriesListPresenter: CountriesListPresenterProtocol {
+//
+//    weak var view: CountriesListProtocol?
+//    private let countryLoader: CountryLoaderProtocol
+//
+//    init(countryLoader: CountryLoaderProtocol) {
+//        self.countryLoader = countryLoader
+//    }
+//
+//    func getData() {
+//        countryLoader.countryDataLoad { [weak self] result in
+//            guard let self else { return }
+//            DispatchQueue.main.async {
+//                switch result {
+//                case .success(let countries):
+//                    self.view?.success(data: countries)
+//                case .failure(let error):
+//                    self.view?.failure(error: error)
+//                }
+//            }
+//        }
+//    }
+//}
 
-    weak var view: CountriesListProtocol?
-    private let countryLoader: CountryLoaderProtocol
+// MARK: Presenter with universal Loader
 
-    init(countryLoader: CountryLoaderProtocol) {
-        self.countryLoader = countryLoader
-    }
-
-    func getData() {
-        countryLoader.countryDataLoad { [weak self] result in
-            guard let self else { return }
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let countries):
-                    self.view?.success(data: countries)
-                case .failure(let error):
-                    self.view?.failure(error: error)
-                }
-            }
-        }
-    }
+protocol CountriesListProtocol: AnyObject {
+    associatedtype DataType: Decodable
+    func success(data: [DataType])
+    func failure(error: Error)
 }
 
+protocol CountriesListPresenterProtocol: AnyObject {
+    associatedtype DataType: Decodable
+    var view: (any CountriesListProtocol)? { get set }
+    func getData()
+}
+
+final class CountriesListPresenter<T: Decodable>: CountriesListPresenterProtocol {
+    typealias DataType = T
+    
+    weak var view: (any CountriesListProtocol)?
+    private let dataLoader: any DataLoadable
+    
+    init(dataLoader: any DataLoadable) {
+        self.dataLoader = dataLoader
+    }
+    func getData() {
+           dataLoader.loadData { [weak self] result in
+               //Member 'loadData' cannot be used on value of type 'any DataLoadable'; consider using a generic constraint instead
+               guard let self = self else { return }
+               DispatchQueue.main.async {
+                   switch result {
+                   case .success(let data):
+                       self.view?.success(data: data)
+                       //Cannot convert value of type '[any Swift.Decodable]' to expected argument type '[any Swift.Decodable]'
+                   case .failure(let error):
+                       self.view?.failure(error: error)
+                   }
+               }
+           }
+       }
+   }
